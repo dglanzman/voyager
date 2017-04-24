e2835d47f89ca62e295fbd9ece6d8fa54c20f446
David Glanzman <gland@seas.upenn.edu>
2017-04-24 04:18:06 -0400

    made inner_product_set compatible with store into same vector

--- linear_algebra.c
+++ linear_algebra.c
@@ -1,5 +1,7 @@
 #include "arithmetic.h"
 #include "linear_algebra.h"
+#include <stdlib.h>
+#include <string.h>
 
 unsigned char inner_product(unsigned char * a, unsigned char * b, size_t len) {
   unsigned char result = 0;
@@ -11,11 +13,15 @@ unsigned char inner_product(unsigned char * a, unsigned char * b, size_t len) {
 
 void inner_product_set(unsigned char * result, unsigned char * A,
   unsigned char * x, size_t in_len, size_t out_len) {
+  unsigned char * x_copy = malloc(in_len);
+  if (!x_copy) abort();
+  memcpy(x_copy, x, in_len);
   for (int i = 0; i < out_len; i++) {
     unsigned char tmp = 0;
     for (int j = 0; j < in_len; j++) {
-      tmp = sum(multiply(A[in_len * i + j], x[j]), tmp);
+      tmp = sum(multiply(A[in_len * i + j], x_copy[j]), tmp);
     }
     result[i] = tmp;
   }
+  free(x_copy);
 }
